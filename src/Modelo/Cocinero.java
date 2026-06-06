070a07140748d7d0eef669a180d8efc7c0a2ee92
NicoCercio12 <cercio1998@gmail.com>
2026-06-06 20:06:46 -0300

    Fixes temporales

--- src/Modelo/Cocinero.java
+++ src/Modelo/Cocinero.java
@@ -2,38 +2,43 @@ package Modelo;
 
 import java.time.LocalDate;
 
-public class Cocinero extends Empleado{
-	//Atributos
+public class Cocinero extends Empleado {
+	// Atributos
 	private String especialidad;
 	private double plusCategoria;
 	private double sueldoBase;
-		//Constructor
+
 	public Cocinero(int idEmpleado, String nombre, String apellido, String dni, LocalDate fechaNacimiento,
-			LocalDate antiguedad, String especialidad, double plusCategoria, double sueldoBase) {
-		super(idEmpleado, nombre, apellido,dni, fechaNacimiento, antiguedad);
+			LocalDate fechaIngreso, String especialidad, double plusCategoria, double sueldoBase) {
+		super(idEmpleado, nombre, apellido, dni, fechaNacimiento, fechaIngreso);
 		this.especialidad = especialidad;
 		this.plusCategoria = plusCategoria;
 		this.sueldoBase = sueldoBase;
 	}
-		// Getters & Setters
+
+	// Getters & Setters
 	public String getEspecialidad() {
 		return especialidad;
 	}
-	
+
 	public void setEspecialidad(String especialidad) {
 		this.especialidad = especialidad;
 	}
+
 	public double getPlusCategoria() {
 		return plusCategoria;
 	}
+
 	public void setPlusCategoria(double plusCategoria) {
 		this.plusCategoria = plusCategoria;
 	}
+
 	public double getSueldoBase() {
 		return sueldoBase;
 	}
+
 	public void setSueldoBase(double sueldoBase) {
 		this.sueldoBase = sueldoBase;
 	}
-	
+
 }
